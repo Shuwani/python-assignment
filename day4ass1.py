cf8b9814e301587bf82bdfc7955f4c39e8f18884
mom1702 <68225758+mom1702@users.noreply.github.com>
2020-07-26 15:50:17 +0530

    Assignment 4

---
+++ day4ass1.py
@@ -0,0 +1,8 @@
+print ("Hello, Dcoder!")
+str1="what we think we become and we will complete our goals "
+u=str1.find("we")
+print("First index:", u)
+v=str1.rfind("we")
+print("Second Index:", v)
+x=str1[6:28].find("we")
+print("Third Index:",x)
